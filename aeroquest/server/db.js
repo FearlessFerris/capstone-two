1b63aaec71f15cc611f61a49a0b5023b14297f49
Marcus <marcus.perry2017@gmail.com>
2024-05-19 10:04:02 -0400

    s

--- aeroquest/server/db.js
+++ aeroquest/server/db.js
@@ -1,49 +1,42 @@
-// Setup Database Connection 
 
-// const { Client } = require( 'pg' );
-// const { connectionString } = require('pg/lib/defaults');
-
-// let DB_URI;
-
-// if( process.env.NODE_ENV === 'test' ){
-//     DB_URI = 'postgresql:///aeroquest_test';
-// }
-// else{
-//     DB_URI = 'postgresql:///aeroquest';
-// }
-
-// let db = new Client({
-//     // connectionString: DB_URI,
-//     username: 'marcus',
-//     password: 'Civil392601*',
-//     host: 'localhost',
-//     database: 'aeroquest',
-//     port: 5432
-// });
-
-// db.connect();
-// module.exports = db;
-
-// Setup Database Connection
-const { Client } = require('pg');
-require('dotenv').config();
+const { Client } = require( 'pg' );
+const { connectionString } = require('pg/lib/defaults');
 
 let DB_URI;
 
-if (process.env.NODE_ENV === 'test') {
+if( process.env.NODE_ENV === 'test' ){
     DB_URI = 'postgresql:///aeroquest_test';
-} else {
-    DB_URI = process.env.DATABASE_URL || 'postgresql:///aeroquest';
+}
+else{
+    DB_URI = 'postgresql:///aeroquest';
 }
 
 let db = new Client({
-    connectionString: DB_URI,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    // connectionString: DB_URI,
+    username: 'marcus',
+    password: 'Civil392601*',
+    host: 'localhost',
+    database: 'aeroquest',
+    port: 5432
 });
 
-db.connect()
-  .then(() => console.log('Connected to the database'))
-  .catch(err => console.error('Database connection error', err.stack));
-
+db.connect();
 module.exports = db;
+
+// Setup Database Connection
+// const { Client } = require('pg');
+// require('dotenv').config();
+
+// console.log('DATABASE_URL:', process.env.DATABASE_URL); // Add this line
+
+// let db = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+// });
+
+// db.connect()
+//     .then(() => console.log('Connected to the database'))
+//     .catch(err => console.error('Database connection error', err.stack));
+
+// module.exports = db;
 
